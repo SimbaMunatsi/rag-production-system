bbc80d07e8c5968f59d0b7927ddf7aa796f4d3f3
SimbaraShe <vsmunatsi@gmail.com>
2026-03-08 15:16:07 +0200

    Langsmith tracing added successfully

--- app/core/config.py
+++ app/core/config.py
@@ -1,22 +1,27 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-
-    # API Keys
+    # Model provider
     OPENAI_API_KEY: str
-    LANGCHAIN_API_KEY: str | None = None
 
-    # Vector Database
+    # LangSmith
+    LANGSMITH_API_KEY: str | None = None
+    LANGSMITH_TRACING: str="true"
+    LANGSMITH_PROJECT: str = "rag-production-system"
+    LANGSMITH_ENDPOINT: str = "https://eu.api.smith.langchain.com"
+    # Add this to your environment variables
+    os.environ["LANGCHAIN_TRACING_COMPRESSION"] = "false"
+    #LANGSMITH_WORKSPACE_ID: str | None = None
+
+    # Vector DB
     CHROMA_DB_PATH: str = "./data/embeddings"
-
-    # LangChain Tracing (optional)
-    LANGCHAIN_TRACING_V2: bool = False
-    LANGCHAIN_PROJECT: str = "rag-production-system"
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        extra="allow",        # prevents crashes if extra env vars exist
+        extra="allow",
         case_sensitive=False
     )
 
